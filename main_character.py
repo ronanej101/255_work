1484a6c7487eec062b3e80d8fb96a77c38341677
ronanej101 <60206562+ronanej101@users.noreply.github.com>
2020-10-15 11:05:24 -0500

    Add files via upload

---
+++ main_character.py
@@ -0,0 +1,17 @@
+#Main character functionality
+
+#Properties (variables)
+#actions (functions)
+
+#Has:
+    #Inventory
+    #Team of pokemon
+    #money
+    #Gym badges
+
+#Actions
+    #Attack
+    #Use an item
+    #Use a pokemon
+    #Talk to an NPC
+    #Run
